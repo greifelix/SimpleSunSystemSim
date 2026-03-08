d8eab46eee5bea21fb318e17cb51e3b86d9e76f2
Felix Greiwe <greifelix@icloud.com>
2026-03-08 12:30:21 +0100

    Fix missing files

---
+++ src/constants_types.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::Vec3;
+use bevy::prelude::*;
+
+pub const SUN_POSITION: Vec3 = Vec3::new(0.0, 2.0, 0.0);
+
+pub const MIN_ZOOM_DISTANCE: f32 = 2.0;
+pub const MAX_ZOOM_DISTANCE: f32 = 50.0;
+pub const START_SIM_SPEED: f32 = 2.0;
+
+#[derive(Resource)]
+pub struct SimulationSpeed(pub f32);
+
+#[derive(Component)]
+pub struct ControlText;
+
+impl Default for SimulationSpeed {
+    fn default() -> Self {
+        SimulationSpeed(START_SIM_SPEED)
+    }
+}
